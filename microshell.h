0440352c29c28a6b74df64225d72c91f3e3e2be9
mingde <mingde@gmail.com>
2025-03-22 19:15:56 +0800

    experimenting with build struct_cmd array

--- microshell.h
+++ microshell.h
@@ -1,11 +1,6 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
-#include <unistd.h>     // fork, execve, chdir, dup, dup2, pipe, close
-#include <sys/wait.h>   // waitpid
-#include <stdlib.h>     // malloc, free, exit
-#include <stdio.h>      // perror
-#include <string.h>     // strcmp, strncmp
-#include <signal.h>     // signal, kill
+
 
 #endif
