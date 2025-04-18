a94ee5af073cf076e33f85c589f953f5b167041a
mingde <myuen@student.42singapore.sg>
2025-04-18 17:22:19 +0800

    edit

--- microshell.c
+++ microshell.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
@@ -6,8 +6,11 @@
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
+*/
 
-int	run(char **av, int start, int in_fd, char **env);
+//int	run(char **av, int start, int in_fd, char **env);
+
+#include "microshell.h"
 
 void	ft_putstr(char* str, char *arg)
 {
@@ -29,7 +32,6 @@ void	fatal_error(void)
 	ft_putstr("error: fatal", NULL);
 	exit (1);
 }
-
 int	is_pipe(char* str)
 {
 	return (strcmp(str, "|") == 0);
@@ -43,6 +45,187 @@ int	is_cd(char* str)
 	return (strcmp(str, "cd") == 0);
 }
 
+int	find_end(char **av, int start)
+{
+	int	i = start;
+
+	while (av[i] && !is_pipe(av[i]) && ! is_break(av[i]))
+	{
+		i++;
+	}
+
+	return (i);
+}
+void	redirection(int got_pipe, int in_fd, int *pipe_fd)
+{
+	if(in_fd != STDIN)
+	{
+		if (dup2(in_fd, STDIN) < 0)
+			fatal_error();
+		close(in_fd);
+	}
+	if (got_pipe)
+	{
+		if (dup2(pipe_fd[1], STDOUT) < 0)
+			fatal_error();
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+	}
+	
+}
+char	**parse_arg(char **av, int start, int end)
+{
+	int	stop = end - start;
+	int	i;
+	char **cmd;
+
+	if (stop <= 0)
+		return (NULL);
+	
+	cmd = malloc(sizeof(char *) * (stop + 1));
+
+	for (i = 0; i < stop; i++)
+		cmd[i] = av[start + i];
+	cmd[stop] = NULL;
+
+	return (cmd);
+}
+
+void	child(char **av, int start, int end, int in_fd, int *pipe_fd, int got_pipe, char **env)
+{
+	redirection(got_pipe, in_fd, pipe_fd);
+	char **cmd = parse_arg(av, start, end);
+	if (!cmd)
+		exit(1);
+	execve(cmd[0], cmd, env);
+	ft_putstr("error: cannot execute ", cmd[0]);
+	free(cmd);
+	exit(1);
+
+}
+
+int parent(char **av, int end, pid_t pid, int in_fd, int *pipe_fd, int got_pipe, char **env)
+{
+	int	status;
+
+	if (in_fd != STDIN)
+		close(in_fd);
+	if (got_pipe)
+	{
+		close(pipe_fd[1]);
+		status = run(av, end + 1, pipe_fd[0], env);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (av[end] && is_break(av[end]))
+			status = run(av, end + 1, STDIN, env);
+	}
+	return (WEXITSTATUS(status));
+}
+int	handle_cd(char **av, int start, int end, char **env)
+{
+	if (end - start != 2)
+	{
+		ft_putstr("error: cd: bad arguments", NULL);
+		return (1);
+	}
+	if (chdir(av[start + 1]) != 0)
+	{
+		ft_putstr("error: cd: cannot change directory to ", av[start + 1]);
+		return (1);
+	}
+	if (av[end])
+	{
+		if (is_pipe(av[end]) || is_break(av[end]))
+		{
+			return (run(av, end + 1, STDIN, env));
+		}
+	}
+	return (0);
+}
+
+int	run(char **av, int start, int in_fd, char **env)
+{
+	int		pipe_fd[2];
+	pid_t	pid;
+	int		status = 0;
+	int		end = find_end(av, start);
+	int		got_pipe = 0;
+
+	if(start >= end)
+		return (0);
+
+	//check cd
+	if (av[start] && is_cd(av[start]))
+	{
+		return(handle_cd(av, start, end, env));
+	}
+
+	//check pipe
+	got_pipe = av[end] && is_pipe(av[end]);
+	if (got_pipe)
+	{
+		if (pipe(pipe_fd) != 0)
+		{
+			fatal_error();
+			return (1);
+		}
+	}
+	//fork
+	pid = fork();
+	if (pid < 0)
+	{
+		fatal_error();
+		return (1);
+	}
+	
+	//child
+	if (pid == 0)
+	{
+		child(av, start, end, in_fd, pipe_fd, got_pipe, env);
+	}
+	//parent
+	else
+	{
+		status = parent(av, end, pid, in_fd, pipe_fd, got_pipe, env);
+	}
+
+	return (status);
+	
+}
+
+int	main (int ac, char **av, char **env)
+{
+	int status = 0;
+
+	if (ac < 2)
+		return (1);
+	status = run(av + 1, 0, STDIN, env);
+	while (waitpid(-1, &status, 0) > 0)
+	;
+
+	return (status);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 int	find_end(char **argv, int start)
 {
 	int i = start;
@@ -196,3 +379,4 @@ int	main (int ac, char **av, char **env)
 	;
 	return(status);
 }
+*/
